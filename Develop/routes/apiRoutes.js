bef569c4841db3bd5313d6fb7ceb94b252019c31
DylanSantiago <2dylansantiago@gmail.com>
2022-11-07 16:12:43 -0500

    api routes created

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+router.get('/', (req, res) => {
+    readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific note
+router.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('../db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+// POST Route for a new note
+router.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in creating note');
+    }
+});
+
+// DELETE Route for a specific tip
+router.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json("Note has been deleted");
+    });
+});
+
+module.exports = router;
