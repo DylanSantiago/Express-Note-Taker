5fce5e46f5183bcd33f89c2aa7cf1cc3e839ef9c
DylanSantiago <2dylansantiago@gmail.com>
2022-11-07 20:25:17 -0500

    BCS helped with file strutcure and now code works as intended

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const api = require('./Develop/routes/index')
+const api = require('./routes/index')
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,11 +11,11 @@ app.use(express.static('public'));
 app.use('/api', api);
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.listen(PORT, () =>
